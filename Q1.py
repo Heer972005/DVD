ff96673f96a16e09d5aa6491c33bd499b991da23
Heer972005 <mehtaheer.972@gmail.com>
2026-03-29 17:42:48 +0530

    adding some designing parts

--- Q1.py
+++ Q1.py
@@ -4,20 +4,24 @@ import seaborn as sns
 
 df = pd.read_csv("Smartphone_Usage_And_Addiction_Analysis_7500_Rows-2.csv")
 
-bins = [15, 25, 35, 50, 100]
+bins = [15, 26, 36, 51, 100]
 labels = ["Youth", "Young Adults", "Adults", "Senior Citizens"]
 
 df["Age Group"] = pd.cut(df["age"], bins=bins, labels=labels)
 
 avg_usage = df.groupby("Age Group")["daily_screen_time_hours"].mean()
 
-plt.bar(avg_usage.index, avg_usage.values, color="skyblue", edgecolor="black")
+plt.bar(avg_usage.index, 
+        avg_usage.values,
+        color="skyblue", 
+        edgecolor="black",
+        alpha=0.7,
+        width=0.6)
 
-
+plt.xticks(rotation=30)
 plt.title("Average Smartphone Usage by Age Group")
 plt.xlabel("Age Group")
 plt.ylabel("Avg Screen Time")
-plt.grid(axis='y')
 
 plt.show()
 
