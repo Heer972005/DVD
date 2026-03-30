11672e68f6a6eabd11f19376901a573f088ac8bd
Heer972005 <mehtaheer.972@gmail.com>
2026-03-30 12:33:22 +0530

    done

--- Q7.py
+++ Q7.py
@@ -2,6 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 df=pd.read_csv("Smartphone_Usage_And_Addiction_Analysis_7500_Rows-2.csv")
-plt.figure(figsize=(7,4))
-sns.pairplot(df, hue="addiction_level")
+
+sns.pairplot(
+    df,
+    hue="addiction_level",
+    diag_kind="kde",
+    palette="Set2",
+    corner=True,
+    height=2.5   # controls size of EACH subplot
+)
+
+plt.tight_layout()
 plt.show()
